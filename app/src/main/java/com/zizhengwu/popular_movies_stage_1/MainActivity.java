7fd4b5451de9b74d2da25a5d135bdc3f7b11c431
Zizheng Wu <me@zizhengwu.com>
2017-01-17 01:31:33 +0800

    with a button

--- app/src/main/java/com/zizhengwu/popular_movies_stage_1/MainActivity.java
+++ app/src/main/java/com/zizhengwu/popular_movies_stage_1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.zizhengwu.popular_movies_stage_1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -24,5 +27,23 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_sort) {
+            Context context = MainActivity.this;
+            String textToShow = "Sort clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
